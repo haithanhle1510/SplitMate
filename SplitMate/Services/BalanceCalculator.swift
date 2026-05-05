60feda2f1738bcd053086cd218cbb980def7d633
haithanhle1510 <haithanh.le@student.uts.edu.au>
2026-05-06 09:11:41 +1000

    fix error

--- SplitMate/Services/BalanceCalculator.swift
+++ SplitMate/Services/BalanceCalculator.swift
@@ -1,18 +1,8 @@
-//
-//  BalanceCalculator.swift
-//  SplitMate
-//
-//  Created by Hai Thanh Le on 27/4/2026.
-//
-
 import Foundation
 
 class BalanceCalculator {
     func getAllExpenses() -> [Expense] {
         return []
-    }
-    func calculateBalances() -> [String: Double] {
-        return 0
     }
 }
 
